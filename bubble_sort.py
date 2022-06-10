283d82ae3b23be1ce7cfcfbb422270888ce59597
Danny Seidel <69514484+DannySeidel@users.noreply.github.com>
2022-06-10 15:31:15 +0200

    added other bubble sort variants

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,12 +1,41 @@
 list = [9, 5, 1, 3, 4, 8, 2, 7, 6]
 
-length = len(list)
-while (length > 1):
-    limiter = 0
-    for index in range(1, length):
-        if list[index - 1] > list[index]:
-            list[index], list[index - 1] = list[index - 1], list[index]
-            limiter = index
-    length = limiter
+def bubble_sort_basic():
+    length = len(list)
+    swapped = False
 
+    for _ in range(length - 1):
+        for index in range(1, length):
+            if list[index - 1] > list[index]:
+                swapped = True
+                list[index], list[index - 1] = list[index - 1], list[index]
+        
+        if not swapped:
+            break
+
+def bubble_sort_optimized():
+    length = len(list)
+    swapped = False
+
+    for _ in range(length - 1):
+        for index in range(1, length):
+            if list[index - 1] > list[index]:
+                swapped = True
+                list[index], list[index - 1] = list[index - 1], list[index]
+        
+        length -= 1
+        if not swapped:
+            break
+
+def bubble_sort_best():
+    length = len(list)
+    while (length > 1):
+        limiter = 0
+        for index in range(1, length):
+            if list[index - 1] > list[index]:
+                list[index], list[index - 1] = list[index - 1], list[index]
+                limiter = index
+        length = limiter
+
+bubble_sort_best()
 print(list)
